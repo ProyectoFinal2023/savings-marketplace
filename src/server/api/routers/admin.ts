9c07631ec2928a8706e119fae2df50e66cb26228
Darío <dariokozicki@gmail.com>
2023-11-02 21:26:56 -0300

    wip

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -8,43 +8,63 @@ import { isAdminMiddleware } from "../middleware/isAdminMiddleware";
 // "https://getdebtbcratest.free.beeceptor.com/get_debt/20112997505";
 
 const CustomerValidator = z.object({
-  data: z.string(),
+  data: z.array(
+    z.object({
+      "Denominacion del deudor": z.string(),
+      Entidad: z.string(),
+      Periodo: z.string(),
+      Situacion: z.string(),
+      Monto: z.string(),
+      "Dias de atraso": z.string(),
+      Observaciones: z.string(),
+    })
+  ),
   contactar: z.string(),
   estado: z.string(),
   riesgo: z.string(),
   CUIT: z.number(),
-  cuotas_impagas: z.number(),
+  cuotas_impagas: z.number().nullish(),
   dev: z.number(),
 });
 
 export type DebtDetail = {
-  'Denominacion del deudor': string,
-  Entidad: string,
-  Periodo: string,
-  Situacion: string,
-  Monto: string,
-  'Dias de atraso': string,
-  Observaciones: string,
-}
+  "Denominacion del deudor": string;
+  Entidad: string;
+  Periodo: string;
+  Situacion: string;
+  Monto: string;
+  "Dias de atraso": string;
+  Observaciones: string;
+};
 
-export type Customer = Omit<z.infer<typeof CustomerValidator>, 'data'> & {
-  hasDebt: boolean,
-  data: DebtDetail
-}[];
+export type Customer = Omit<z.infer<typeof CustomerValidator>, "data"> &
+  {
+    hasDebt: boolean;
+    data: DebtDetail;
+  }[];
 
 export const adminRouter = createTRPCRouter({
   getByCuit: publicProcedure
     .use(isAdminMiddleware)
     .input(z.object({ cuit: z.string() }))
     .query(async ({ input: { cuit } }) => {
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       const response = await fetch(`${env.WAREHOUSE_URL}/get_debt/${cuit}`);
       // perhaps some error handling
       if (!response.ok) {
-        throw new TRPCError({ message: "Warehouse is down!", code: "INTERNAL_SERVER_ERROR" });
+        throw new TRPCError({
+          message: "Warehouse is down!",
+          code: "INTERNAL_SERVER_ERROR",
+        });
       }
 
       // should prob validate the shape with Zod
       const validated = CustomerValidator.parse(await response.json());
-      return validated.dev ? { ...validated, data: (JSON.parse(validated.data) as Array<DebtDetail>), hasDebt: true } : { ...validated, data: [], hasDebt: false, cuotas_impagas: 0 };
+      return validated.dev
+        ? {
+            ...validated,
+            hasDebt: true,
+          }
+        : { ...validated, data: [], hasDebt: false, cuotas_impagas: 0 };
     }),
 });
